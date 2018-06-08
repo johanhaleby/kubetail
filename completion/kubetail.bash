81d4aa0bb5d62b473cfcaa20fcd1d74f021baef3
Olivier Gerbron <olivier.gerbron@chauffeur-prive.com>
2018-06-08 14:16:05 +0200

    Add completion scripts and corresponding documentation

---
+++ completion/kubetail.bash
@@ -0,0 +1,9 @@
+_kubetail()
+{
+  local curr_arg;
+  curr_arg=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "$(kubectl get pods --no-headers | awk '{print $1}')" -- $curr_arg ) );
+}
+
+complete -F _kubetail kubetail
+
